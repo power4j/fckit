6c29053fc67a5a4784d714969d62e0c9d7eab863
cj <power4j@outlook.com>
2022-01-12 10:13:04 +0800

    feat: introduce Validator

---
+++ fist-kit-boot-parent/fist-kit-boot-data/src/main/java/com/power4j/fist/data/crud/service/validator/DeleteValidator.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2021 ChenJun (power4j@outlook.com & https://github.com/John-Chan)
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.gnu.org/licenses/lgpl.html
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.power4j.fist.data.crud.service.validator;
+
+/**
+ * @author CJ (power4j@outlook.com)
+ * @date 2022/1/12
+ * @since 1.0
+ */
+public interface DeleteValidator<T> {
+
+	/**
+	 * 验证删除操作
+	 * @param object 入参数据
+	 */
+	void deleteValidate(T object);
+
+}
