9bfa7cc985876a93d72e09437fabbdb7b8831ae6
cj <power4j@outlook.com>
2022-05-31 13:32:04 +0800

    feat: jackson自定义设置可配置

--- fist-kit-boot-parent/fist-kit-boot-core/src/main/java/com/power4j/fist/boot/common/jackson/JacksonConfig.java
+++ fist-kit-boot-parent/fist-kit-boot-core/src/main/java/com/power4j/fist/boot/common/jackson/JacksonConfig.java
@@ -16,39 +16,96 @@
 
 package com.power4j.fist.boot.common.jackson;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.power4j.coca.kit.common.datetime.DateTimePattern;
 import com.power4j.fist.boot.common.jackson.module.DateTimeModule;
 import com.power4j.fist.boot.common.jackson.module.NumberStrModule;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.time.ZoneId;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.TimeZone;
+import java.util.stream.Collectors;
 
 /**
  * @author CJ (power4j@outlook.com)
  * @date 2021/12/16
  * @since 1.0
  */
+@Slf4j
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(ObjectMapper.class)
+@ConditionalOnProperty(value = JacksonCustomizeProperties.PROP_PREFIX, name = "enabled", matchIfMissing = true)
+@EnableConfigurationProperties(JacksonCustomizeProperties.class)
 @AutoConfigureBefore(JacksonAutoConfiguration.class)
+@RequiredArgsConstructor
 public class JacksonConfig {
+
+	private final JacksonCustomizeProperties jacksonCustomizeProperties;
+
+	private final static Map<JacksonCustomizeProperties.ModuleName, Module> MODULE_MAP = new HashMap<>(2);
+	static {
+		MODULE_MAP.put(JacksonCustomizeProperties.ModuleName.Date, new DateTimeModule());
+		MODULE_MAP.put(JacksonCustomizeProperties.ModuleName.NumberToStr, new NumberStrModule());
+	}
 
 	@Bean
 	public Jackson2ObjectMapperBuilderCustomizer customizer() {
 		return builder -> {
 			builder.locale(Locale.CHINA);
-			builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
-			builder.simpleDateFormat(DateTimePattern.DATETIME);
-			builder.modulesToInstall(new NumberStrModule(), new DateTimeModule());
+			applyTimeZone(builder);
+			applySimpleDateFormat(builder);
+			applyModules(builder);
 		};
+	}
+
+	private void applyTimeZone(Jackson2ObjectMapperBuilder builder) {
+		final String timeZoneId = jacksonCustomizeProperties.getTimeZoneId();
+		if (StringUtils.isEmpty(timeZoneId)) {
+			return;
+		}
+		log.info("Set time zone :{}", timeZoneId);
+		if (JacksonCustomizeProperties.TZ_SYSTEM.equalsIgnoreCase(timeZoneId)) {
+			builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
+			return;
+		}
+		builder.timeZone(TimeZone.getTimeZone(ZoneId.of(timeZoneId)));
+	}
+
+	private void applySimpleDateFormat(Jackson2ObjectMapperBuilder builder) {
+		final String format = jacksonCustomizeProperties.getSimpleDateFormat();
+		if (!StringUtils.isEmpty(format)) {
+			log.info("Set simple date format :{}", format);
+			builder.simpleDateFormat(format);
+		}
+	}
+
+	private void applyModules(Jackson2ObjectMapperBuilder builder) {
+		List<JacksonCustomizeProperties.ModuleName> excludeModules = ObjectUtils
+				.defaultIfNull(jacksonCustomizeProperties.getExcludeModules(), Collections.emptyList());
+		List<Module> modules = MODULE_MAP.entrySet().stream().filter(kv -> !excludeModules.contains(kv.getKey()))
+				.map(Map.Entry::getValue).collect(Collectors.toList());
+		if (!modules.isEmpty()) {
+			List<String> names = modules.stream().map(Module::getModuleName).collect(Collectors.toList());
+			log.info("Install modules: {}", names);
+			builder.modulesToInstall(modules.toArray(modules.toArray(new Module[0])));
+		}
 	}
 
 }
---
+++ fist-kit-boot-parent/fist-kit-boot-core/src/main/java/com/power4j/fist/boot/common/jackson/JacksonCustomizeProperties.java
@@ -0,0 +1,61 @@
+package com.power4j.fist.boot.common.jackson;
+
+import com.power4j.coca.kit.common.datetime.DateTimePattern;
+import com.power4j.fist.boot.common.prop.PropConstant;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.lang.Nullable;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author CJ (power4j@outlook.com)
+ * @date 2022/5/31
+ * @since 1.0
+ */
+@Data
+@ConfigurationProperties(prefix = JacksonCustomizeProperties.PROP_PREFIX)
+public class JacksonCustomizeProperties {
+
+	public static final String PROP_ENTRY = "jackson.customize";
+
+	public static final String PROP_PREFIX = PropConstant.PROP_ROOT_PREFIX + PROP_ENTRY;
+
+	public static final String TZ_SYSTEM = "system";
+
+	/**
+	 * 是否启用个性化配置
+	 */
+	private boolean enabled = true;
+
+	/**
+	 * 排除的自定义模块
+	 */
+	@Nullable
+	private List<ModuleName> excludeModules = Collections.emptyList();
+
+	/**
+	 * 设置默认时间格式,留空则使用Jackson默认值
+	 */
+	@Nullable
+	private String simpleDateFormat = DateTimePattern.DATETIME;
+
+	/**
+	 * 设置默认时区,比如 {@code 'Asia/Shanghai'},特殊值 system 表示使用系统时区,留空则使用Jackson默认值(UTC)
+	 * @see <a
+	 * href=https://docs.oracle.com/cd/E84527_01/wcs/tag-ref/MISC/TimeZones.html>TimeZones</url>
+	 */
+	@Nullable
+	private String timeZoneId = TZ_SYSTEM;
+
+	public enum ModuleName {
+
+		/** 日期、时间相关 */
+		Date,
+		/** 大整数转String */
+		NumberToStr
+
+	}
+
+}
