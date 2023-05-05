006b97c79e6dae0f7ca33174255c3be82c9078f9
cj <power4j@outlook.com>
2023-05-05 14:03:45 +0800

    test: Add user decode test

--- fist-kit-app/fist-security/fist-support-security/src/test/java/com/power4j/fist/boot/security/inner/DefaultUserCodecTest.groovy
+++ fist-kit-app/fist-security/fist-support-security/src/test/java/com/power4j/fist/boot/security/inner/DefaultUserCodecTest.groovy
@@ -85,4 +85,17 @@ class DefaultUserCodecTest extends Specification {
 		then:
 		userInfo.getUsername() == decoded.getUsername()
 	}
+
+	def "Decode"(){
+		String value = "+gz H4sIAAAAAAAA/3VUy27bMBD8lYJnE0KvuqWADwGKFqibUxAEG3KtEuKrJGXDCPLvXVK2U4nUTZjZmZ1dUnxnU8TwKFn/dVc+LRhkPQvOJbZjVonxx4wcLjGh+fIgjbLEwAkShKegWW8nrXfMYALWvzOJPt0woRXalN3ZUcXEIYv5Gd/IwGMwKkblbGT9M/FgRBcvscsGXKKmmk8wOI0VaNBOFZiH4B4GrNF1KXjFlT26WzkRd1y7oXYp0U4Kz3WMqrYk1jR1I91Z8oARU22TW6+JjHnaZ20EUtYgStUyXpWWUZpgW1+hZb5CrfN+Ms1TbMYpTBO8NvYgvOlg7PJKb9ueQQNiRunM4ga1QJPs/k4YLndAOIlbFtSScnG6uC5w8QfFuKZAiJn+n6DRMWFbsOkSpzeznPZqUxNR2QEbgegwOIw8qsHiItDkJVSB8n5knnsDrvtebXKLJVdW7WrNHa9beKAL26qfiVoQcNhAGyb0h02m7VKIhmSy2omxIbkSKwndI0Gj4fIenY4X9jI/pgc3BZEfT1JD/hXyjS7P3a+f3/evh8ff+9dvD4c9e/n4+AehQWBLigUAAA==";
+
+		DefaultUserCodec codec = new DefaultUserCodec()
+
+		when:
+		UserInfo decoded = codec.decode(value)
+
+		then:
+		"root" == decoded.getUsername()
+		1 == decoded.getUserId()
+	}
 }
